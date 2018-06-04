e3bd44d42979af19d6a5f3f2b835bbf803c6ae69
Andrew Jack <andrew.jack@drivetribe.com>
2018-06-04 12:14:05 +0100

    Remove old camera code from JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import Camera from './Camera';
+// @flow
+
 import RNCamera from './RNCamera';
-import FaceDetector from './FaceDetector';
-
-export { RNCamera, FaceDetector };
-
-export default Camera;
+export { RNCamera };
